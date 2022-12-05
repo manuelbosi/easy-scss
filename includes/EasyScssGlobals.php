3e7cbc938eca4b02cbcb40413beabe802bb2d80b
Manuel Bosi <manuel.bosi@hinto.com>
2022-12-05 23:03:51 +0100

    feat(shared): add database globals variables

--- includes/EasyScssGlobals.php
+++ includes/EasyScssGlobals.php
@@ -10,4 +10,9 @@ class EasyScssGlobals {
     static $plugin_version = "1.0.0";
     static $plugin_name = "easy-scss";
 
+	// Database
+	static $option_entrypoint_key = 'entrypoint_file';
+	static $option_destination_key = 'destination_folder';
+	static $table_prefix = 'easy_scss_';
+	static $options_table =  'options';
 }
