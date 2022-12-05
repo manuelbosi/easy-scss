a87e46468bccac523d10d56c60968e11817f3f1a
Manuel Bosi <manuel.bosi@hinto.com>
2022-12-05 23:07:11 +0100

    feat(shared): add create plugin options table action on plugin activation

--- includes/EasyScssActivator.php
+++ includes/EasyScssActivator.php
@@ -8,7 +8,23 @@ class EasyScssActivator {
      * Fired during plugin activation.
      */
 	public static function activate() {
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+		(new EasyScssActivator())->create_table_easy_scss_options();
+	}
 
+	// wp_eas_options table
+	private function create_table_easy_scss_options() {
+		global $wpdb;
+		$table = $wpdb->prefix . EasyScssGlobals::$table_prefix . EasyScssGlobals::$options_table ;
+		$query = "
+            CREATE TABLE IF NOT EXISTS `".$table."` (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `meta_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
+              `meta_value` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci
+        ";
+		dbDelta($query);
 	}
 
 }
