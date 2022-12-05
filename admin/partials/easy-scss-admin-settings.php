c7f4fd4bb052749de145fd34ef92be4339c49d4c
Manuel Bosi <manuel.bosi@hinto.com>
2022-12-05 23:37:21 +0100

    feat(admin): add admin notices feedback on plugin settings form

--- admin/partials/easy-scss-admin-settings.php
+++ admin/partials/easy-scss-admin-settings.php
@@ -2,8 +2,10 @@
 
 use EasyScss\Shared\EasyScssGlobals;
 use EasyScss\Shared\EasyScssOptionsManager;
+use EasyScss\Shared\EasyScssUtils;
 
 $option_manager = new EasyScssOptionsManager();
+$utils = new EasyScssUtils();
 
 $entry_key = EasyScssGlobals::$option_entrypoint_key;
 $destination_key = EasyScssGlobals::$option_destination_key;
@@ -11,9 +13,26 @@ $destination_key = EasyScssGlobals::$option_destination_key;
 $entrypoint_file_option = $option_manager->get($entry_key);
 $destination_folder_option = $option_manager->get($destination_key);
 
+$has_errors = isset($_GET['error']) && $_GET['error'] === "1";
+$is_saved = isset($_GET['saved']) && $_GET['saved'] === "1";
+$has_empty_values = isset($_GET['empty']) && $_GET['empty'] === "1";
+
 ?>
 
 <div class="wrap easy-scss">
+
+  <?php
+    if ($has_errors) {
+        $utils->admin_notice('error', 'There was an error while saving settings. Try Again.');
+    }
+    if ($is_saved) {
+        $utils->admin_notice('success', 'Settings saved successfully.');
+    }
+    if ($has_empty_values) {
+        $utils->admin_notice('error', 'All fields must not be empty.');
+    }
+  ?>
+
   <div class="page-title">
     <h1><?php echo esc_html__('Settings') ?></h1>
     <h4><?php echo esc_html__('All path must be relative to the active theme folder.') ?></h4>
